d83874223560517928a671c2bb346b284cab6e4c
Valeriu Balan <balan.valeriu@live.com>
2017-11-06 02:16:05 +0200

    refact: Reoved contact action

--- src/Anvyl.JsonLocalizer.Presentation/Controllers/HomeController.cs
+++ src/Anvyl.JsonLocalizer.Presentation/Controllers/HomeController.cs
@@ -29,13 +29,6 @@ namespace Anvyl.JsonLocalizer.Presentation.Controllers
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
