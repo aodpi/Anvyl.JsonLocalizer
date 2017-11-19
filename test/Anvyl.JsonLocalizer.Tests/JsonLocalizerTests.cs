c59428923c6fc25df8f53cb0b8aa1124d4587da1
aodpi <balan.valeriu@live.com>
2017-11-19 22:42:05 +0200

    feat: Added documentation, refactored tests

--- test/Anvyl.JsonLocalizer.Tests/JsonLocalizerTests.cs
+++ test/Anvyl.JsonLocalizer.Tests/JsonLocalizerTests.cs
@@ -1,9 +1,7 @@
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
-using System;
 using System.Globalization;
-using System.IO;
 using System.Linq;
 using Xunit;
 
@@ -12,23 +10,24 @@ namespace Anvyl.JsonLocalizer.Tests
     public class JsonLocalizerTests
     {
         private readonly IStringLocalizer _localizer;
-        private const string cacheKeyPrefix = "__loc__";
-        private const string resourcesPath = "Localization";
+        private const string CacheKeyPrefix = "__loc__";
+        private const string ResourcesPath = "Localization";
         private readonly IDistributedCache _cache;
 
         public JsonLocalizerTests()
         {
-            ServiceProvider provider;
-            ServiceCollection services = new ServiceCollection();
+            var services = new ServiceCollection();
             services.AddDistributedMemoryCache();
             services.Configure<JsonLocalizerOptions>(opts =>
             {
-                opts.CacheKeyPrefix = cacheKeyPrefix;
-                opts.ResourcesPath = resourcesPath;
+                opts.CacheKeyPrefix = CacheKeyPrefix;
+                opts.ResourcesPath = ResourcesPath;
             });
             services.AddSingleton<IStringLocalizerFactory, JsonStringLocalizerFactory>();
-            provider = services.BuildServiceProvider();
+            
+            var provider = services.BuildServiceProvider();
             var factory = provider.GetRequiredService<IStringLocalizerFactory>();
+            
             _localizer = factory.Create(null);
             _cache = provider.GetRequiredService<IDistributedCache>();
         }
@@ -36,7 +35,7 @@ namespace Anvyl.JsonLocalizer.Tests
         [Fact(DisplayName = "Show [<key>] whenever the <key> does not exist in json file")]
         public void Returns_Resource_NotFound_With_Square_Brackets()
         {
-            string locKey = "Hello Traveller";
+            const string locKey = "Hello Traveller";
             Assert.NotNull(_localizer);
             Assert.NotNull(_localizer[locKey]);
             Assert.True(_localizer[locKey].ResourceNotFound);
@@ -46,7 +45,7 @@ namespace Anvyl.JsonLocalizer.Tests
         [Fact(DisplayName = "Returns a resource from CurrentCulture named json file")]
         public void ReturnsResource()
         {
-            string locKey = "Hello";
+            const string locKey = "Hello";
             Assert.NotNull(_localizer);
             Assert.NotNull(_localizer[locKey]);
             Assert.True(!_localizer[locKey].ResourceNotFound);
@@ -56,33 +55,33 @@ namespace Anvyl.JsonLocalizer.Tests
         [Fact(DisplayName = "Returns a resource that should be cached afterwards")]
         public void Returns_Resource_And_Is_Saved_In_Cache()
         {
-            string locKey = "Hello";
+            const string locKey = "Hello";
             Assert.NotNull(_localizer);
             Assert.NotNull(_localizer[locKey]);
             Assert.True(!_localizer[locKey].ResourceNotFound);
             Assert.Equal("Noroc", _localizer[locKey].Value);
-            Assert.Equal("Noroc", _cache.GetString($"{cacheKeyPrefix}_{locKey}"));
+            Assert.Equal("Noroc", _cache.GetString($"{CacheKeyPrefix}_{locKey}"));
 
             // Cleanup
-            _cache.Remove($"{cacheKeyPrefix}_{locKey}");
+            _cache.Remove($"{CacheKeyPrefix}_{locKey}");
         }
 
         [Fact(DisplayName = "If the key does not exist it should not be saved in cache")]
         public void Unexistent_Value_Is_Not_Saved_In_Cache()
         {
-            string locKey = "Hello Traveller";
+            const string locKey = "Hello Traveller";
             Assert.NotNull(_localizer);
             Assert.NotNull(_localizer[locKey]);
             Assert.True(_localizer[locKey].ResourceNotFound);
             Assert.Equal($"[{locKey}]", _localizer[locKey].Value);
-            Assert.Null(_cache.GetString($"{cacheKeyPrefix}_{locKey}"));
+            Assert.Null(_cache.GetString($"{CacheKeyPrefix}_{locKey}"));
         }
 
         [Fact(DisplayName = "Creates new file for new culture with null values")]
         public void ThrowsIOExceptionForMissingJson()
         {
             var localizerRO = _localizer.WithCulture(new CultureInfo("ro-RO"));
-            string locKey = "Hello";
+            const string locKey = "Hello";
             Assert.NotNull(localizerRO);
             Assert.True(localizerRO[locKey].ResourceNotFound);
             Assert.Equal($"[{locKey}]", localizerRO[locKey]);
@@ -93,10 +92,10 @@ namespace Anvyl.JsonLocalizer.Tests
         [Fact(DisplayName = "Test GetAllStrings")]
         public void GetAllStrings()
         {
-            var allStrings = _localizer.GetAllStrings();
-            Assert.All(allStrings, (ls) => Assert.False(ls.ResourceNotFound));
-            string[] names = allStrings.Select(ls => ls.Name).ToArray();
-            string[] values = allStrings.Select(ls => ls.Value).ToArray();
+            var localizedStrings = _localizer.GetAllStrings().ToArray();
+            Assert.All(localizedStrings, ls => Assert.False(ls.ResourceNotFound));
+            var names = localizedStrings.Select(ls => ls.Name).ToArray();
+            var values = localizedStrings.Select(ls => ls.Value).ToArray();
             Assert.Contains("Hello", names);
             Assert.Contains("Hello {0}", names);
             Assert.Contains("Noroc", values);
