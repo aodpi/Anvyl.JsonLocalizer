7654a990a0691c60683ff9b7f42092ccbbc145ba
aodpi <balan.valeriu@live.com>
2017-11-19 21:54:31 +0200

    feat: Added some tests

--- test/Anvyl.JsonLocalizer.Tests/UnitTest1.cs
+++ test/Anvyl.JsonLocalizer.Tests/UnitTest1.cs
@@ -2,34 +2,106 @@ using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
 using System;
+using System.Globalization;
+using System.Linq;
 using Xunit;
 
 namespace Anvyl.JsonLocalizer.Tests
 {
-    public class UnitTest1
+    public class JsonLocalizerTests
     {
-        private IServiceProvider _provider;
-        public UnitTest1()
+        private readonly IStringLocalizer _localizer;
+        private const string cacheKeyPrefix = "__loc__";
+        private const string resourcesPath = "Localization";
+        private readonly IDistributedCache _cache;
+
+        public JsonLocalizerTests()
         {
+            ServiceProvider provider;
             ServiceCollection services = new ServiceCollection();
             services.AddDistributedMemoryCache();
             services.Configure<JsonLocalizerOptions>(opts =>
             {
-                opts.CacheKeyPrefix = "__loc__";
-                opts.ResourcesPath = "Localization";
+                opts.CacheKeyPrefix = cacheKeyPrefix;
+                opts.ResourcesPath = resourcesPath;
             });
             services.AddSingleton<IStringLocalizerFactory, JsonStringLocalizerFactory>();
-            _provider = services.BuildServiceProvider();
+            provider = services.BuildServiceProvider();
+            var factory = provider.GetRequiredService<IStringLocalizerFactory>();
+            _localizer = factory.Create(null);
+            _cache = provider.GetRequiredService<IDistributedCache>();
         }
-        [Fact]
-        public void Test1()
+
+        [Fact(DisplayName = "Show [<key>] whenever the <key> does not exist in json file")]
+        public void Returns_Resource_NotFound_With_Square_Brackets()
         {
-            const string locKey = "Hello";
-            var srv = _provider.GetRequiredService<IStringLocalizerFactory>();
-            var localizer = srv.Create(null);
-            Assert.NotNull(localizer);
-            Assert.NotNull(localizer[locKey]);
-            Assert.True(localizer[locKey].ResourceNotFound);
+            string locKey = "Hello Traveller";
+            Assert.NotNull(_localizer);
+            Assert.NotNull(_localizer[locKey]);
+            Assert.True(_localizer[locKey].ResourceNotFound);
+            Assert.Equal($"[{locKey}]", _localizer[locKey].Value);
+        }
+
+        [Fact(DisplayName = "Returns a resource from CurrentCulture named json file")]
+        public void ReturnsResource()
+        {
+            string locKey = "Hello";
+            Assert.NotNull(_localizer);
+            Assert.NotNull(_localizer[locKey]);
+            Assert.True(!_localizer[locKey].ResourceNotFound);
+            Assert.Equal("Noroc", _localizer[locKey].Value);
+        }
+
+        [Fact(DisplayName = "Returns a resource that should be cached afterwards")]
+        public void Returns_Resource_And_Is_Saved_In_Cache()
+        {
+            string locKey = "Hello";
+            Assert.NotNull(_localizer);
+            Assert.NotNull(_localizer[locKey]);
+            Assert.True(!_localizer[locKey].ResourceNotFound);
+            Assert.Equal("Noroc", _localizer[locKey].Value);
+            Assert.Equal("Noroc", _cache.GetString($"{cacheKeyPrefix}_{locKey}"));
+
+            // Cleanup
+            _cache.Remove($"{cacheKeyPrefix}_{locKey}");
+        }
+
+        [Fact(DisplayName = "If the key does not exist it should not be saved in cache")]
+        public void Unexistent_Value_Is_Not_Saved_In_Cache()
+        {
+            string locKey = "Hello Traveller";
+            Assert.NotNull(_localizer);
+            Assert.NotNull(_localizer[locKey]);
+            Assert.True(_localizer[locKey].ResourceNotFound);
+            Assert.Equal($"[{locKey}]", _localizer[locKey].Value);
+            Assert.Null(_cache.GetString($"{cacheKeyPrefix}_{locKey}"));
+        }
+
+        [Fact(DisplayName = "Throw exception when json file for culture does not exist")]
+        public void ThrowsIOExceptionForMissingJson()
+        {
+            var localizerRO = _localizer.WithCulture(new CultureInfo("ro-RO"));
+            string locKey = "Hello";
+            Assert.NotNull(localizerRO);
+            Assert.Throws<System.IO.FileNotFoundException>(() =>
+            {
+                var roString = localizerRO[locKey];
+            });
+            // Cleanup
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+        }
+
+        [Fact(DisplayName = "Test GetAllStrings")]
+        public void GetAllStrings()
+        {
+            var allStrings = _localizer.GetAllStrings();
+            Assert.All(allStrings, (ls) => Assert.False(ls.ResourceNotFound));
+            string[] names = allStrings.Select(ls => ls.Name).ToArray();
+            string[] values = allStrings.Select(ls => ls.Value).ToArray();
+            Assert.Contains("Hello", names);
+            Assert.Contains("Hello {0}", names);
+            Assert.Contains("Noroc", values);
+            Assert.Contains("Noroc {0}", values);
         }
     }
 }
